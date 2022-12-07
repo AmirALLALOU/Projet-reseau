d5875075f38889fe838f2bcf6f8338fa34513731
amirosxx7 <40476257+amirosxx7@users.noreply.github.com>
2022-12-07 21:15:51 +0100

    Add files via upload

---
+++ ig.py
@@ -0,0 +1,51 @@
+import tkinter as tk
+
+liste = []
+sousliste = []
+#parcours le fichier et ajoute a la liste chaque element separes par un : sans le retour a la ligne chaque ligne est une sous liste
+with open("test.txt", "r") as f:
+    for line in f:
+        sousliste = line.split(":")
+        sousliste[-1] = sousliste[-1].rstrip()
+        liste.append(sousliste)
+print("liste:",liste)
+
+#liste des IP
+listeIP = []
+for i in range(len(liste)):
+    #verifie si l'element n'est pas deja dans la liste
+    if liste[i][1] not in listeIP:
+        listeIP.append(liste[i][1])
+    if liste[i][2] not in listeIP:
+        listeIP.append(liste[i][2])
+print("listeIP:",listeIP)
+
+
+#differente liste pour chaque element
+listeIPsource = []
+listeIPdestination = []
+listePortSource = []
+listePortDestination = []
+listeProtocole = []
+
+for i in range(len(liste)):
+    listeProtocole.append(liste[i][0])
+    listeIPsource.append(liste[i][1])
+    listeIPdestination.append(liste[i][2])
+    #listePortSource.append(liste[i][3])
+    #listePortDestination.append(liste[i][4])
+
+
+#creer une fenetre
+window = tk.Tk()
+window.title("Flow graph")
+window.geometry("1080x720")
+
+#affiche les elements de la listeIP en haut sur la meme ligne a l'horizontal et trace un trait en dessous de chaque element allant vers le bas
+for i in range(len(listeIP)):
+    tk.Label(window, text=listeIP[i]).grid(row=0, column=i)
+
+
+
+
+window.mainloop()
